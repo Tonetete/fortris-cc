b4c429267b50ffcd9e24d28bfa056a7229c50471
Antonio Rivas <toneterivas@gmail.com>
2023-07-17 23:42:04 +0200

    add mongoose compatibility for mongodb access and configuration URI

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountsController } from './accounts/accounts.controller';
+
+import * as settings from '../../settings.json';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    MongooseModule.forRoot(settings['DATABASE_URI']),
+  ],
+  controllers: [AppController, AccountsController],
   providers: [AppService],
 })
 export class AppModule {}
