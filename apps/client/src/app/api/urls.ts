2de25aa7cc8cc21803380e934faa7e8a38a903e3
Antonio Rivas <toneterivas@gmail.com>
2023-07-18 23:05:34 +0200

    api urls and adding material

---
+++ apps/client/src/app/api/urls.ts
@@ -0,0 +1,11 @@
+const getApiBaseUrl = () => `http://localhost:3000`;
+
+export const getAccountsBaseUrl = () => `${getApiBaseUrl()}/account`;
+export const getTransactionBaseUrl = () => `${getApiBaseUrl()}/transactions`;
+
+export const getAccountsUrl = () => `${getAccountsBaseUrl()}`;
+export const getAccountUrlById = (id: string) => `${getAccountsBaseUrl()}/${id}`;
+
+export const getTransactionsUrl = () => `${getTransactionBaseUrl()}`;
+export const getTransactionsUrlById = (id: string) => `${getTransactionBaseUrl()}/${id}`;
+export const getTransactionsUrlByAccountId = (account_id: string) => `${getTransactionBaseUrl()}/account_id/${account_id}`;
