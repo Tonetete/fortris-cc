a24b0a2d5e12a2a96e3297a108a195cd0cc83532
Antonio Rivas <toneterivas@gmail.com>
2023-07-20 00:35:18 +0200

    add breadcrumb component and refactor header

--- apps/client/src/app/components/header/header.component.ts
+++ apps/client/src/app/components/header/header.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRouteSnapshot, NavigationEnd, Router } from '@angular/router';
-import { svgBitcoinLogo } from '../../../assets/svg-logo';
-import { USDBTCPrice } from '@fortris-cc/types';
 import { filter, map } from 'rxjs';
-import { TrackerService } from '../../services/tracker.service';
 
 @Component({
   selector: 'fortris-header',
@@ -14,14 +9,9 @@ import { TrackerService } from '../../services/tracker.service';
 })
 export class HeaderComponent {
   title = '';
-  decodedSymbol: SafeHtml | undefined;
-  USDBTCPrice: USDBTCPrice | null = null;
 
   constructor(
     private router: Router,
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer,
-    private trackerService: TrackerService
   ) {
     
     this.router.events
@@ -34,22 +24,6 @@ export class HeaderComponent {
       .subscribe((title: string) => {
         this.title = title;
       });
-
-      this.registryIcon('btc-icon', svgBitcoinLogo);
-  }
-
-  ngOnInit() {
-    this.trackerService.getUSDBTCPriceMessage();
-    this.trackerService.USDBTCPrice$.subscribe((price: USDBTCPrice) => {
-      this.USDBTCPrice = price;
-    });
-  }
-
-  registryIcon(name: string, path: string) {
-    this.matIconRegistry.addSvgIconLiteral(
-      name,
-      this.domSanitizer.bypassSecurityTrustHtml(path)
-    );
   }
 
   private findRouteWithTitle(route: ActivatedRouteSnapshot): string {
---
+++ apps/client/src/app/services/breadcrumb.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BreadcrumbPath } from '@fortris-cc/types';
+import {  ReplaySubject, filter } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BreadcrumbService {
+  private readonly _breadcrumbPathSubject: ReplaySubject<BreadcrumbPath> =
+    new ReplaySubject<BreadcrumbPath>(0);
+  readonly breadcrumbPathBehaviourSubject$ = this._breadcrumbPathSubject
+    .asObservable()
+    .pipe(filter((value) => !!value));
+
+  constructor() {}
+
+  setBreadcrumbPath(url: string, router: Router) {
+    const breadcrumbPath: BreadcrumbPath = [];
+    const paths = url.split('/');
+    paths.forEach((p) => {
+      const routeConfig = router.config.find((r) => p === r.data?.['breadcrumb']?.['path']);
+      if (routeConfig) {
+        const alias = routeConfig.data?.['title'] as string;
+        const path = routeConfig.path as string;
+
+        breadcrumbPath.push({ path, alias });
+      }
+    });
+    this._breadcrumbPathSubject.next(breadcrumbPath);
+  }
+}
